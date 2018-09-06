c3b81d096947075912218b87903d7c094888bc60
Connor Schönberner <connor@todatec.de>
2018-09-06 11:53:00 +0200

    HRS3105B-41: Anfang gemacht

--- SWARMComposerApp/app/src/main/java/swarm/swarmcomposerapp/Model/CompNode.java
+++ SWARMComposerApp/app/src/main/java/swarm/swarmcomposerapp/Model/CompNode.java
@@ -1,4 +1,33 @@
 package swarm.swarmcomposerapp.Model;
 
+/**
+ * A CompNode represents a Service within the drawing of a Composition.
+ */
 public class CompNode {
+
+    private String serviceName;
+    private String info;
+    private long imgID;
+    private int posX;
+    private int posY;
+
+    /**
+     * Creates a CompNode with all basic information needed for drawing it.
+     *
+     * @param posX - X position in the drawing
+     * @param posY - Y position in the drawing
+     * @param info - info text of the service
+     * @param imgID - ID of the image
+     * @param serviceName - name of the service
+     */
+    public CompNode(int posX, int posY, String info, long imgID, String serviceName){
+        this.posX = posX;
+        this.posY = posY;
+        this.info = info;
+        this.imgID = imgID;
+        this.serviceName = serviceName;
+    }
+
+
+
 }
--- SWARMComposerApp/app/src/main/java/swarm/swarmcomposerapp/Model/Composition.java
+++ SWARMComposerApp/app/src/main/java/swarm/swarmcomposerapp/Model/Composition.java
@@ -1,4 +1,17 @@
 package swarm.swarmcomposerapp.Model;
 
+import java.util.List;
+
 public class Composition {
+
+    //private long authorID;
+    private String author;
+    private long lastChange;
+    private long created;
+    List<CompNode> compNodeList;
+    List<CompEdge> compEdgeList;
+
+
+
+
 }
