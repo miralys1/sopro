ce6f54393f487be99747c1d2806e014c3ee285ff
Jette <klette-99@kabelmail.de>
2018-09-08 15:23:00 +0200

    HRS3105B-45: Compatibility Klasse

---
+++ software/backend/src/main/java/de/sopro/model/Compatibility.java
@@ -0,0 +1,9 @@
+package de.sopro.model;
+
+public abstract class Compatibility {
+
+	public static CompatibilityAnswer checkCompatibility(long ServiceID1, long ServiceID2) {
+		// TODO: implement
+		return null;
+	}
+}
