5b7b8588acb13b258bed0011a2f4562474edb970
Janek Haberer <j.haberer@gmx.net>
2018-09-10 15:27:08 +0200

    Added boilerplate Loginpage

--- software/frontend/src/components/Login.vue
+++ software/frontend/src/components/Login.vue
@@ -1,64 +1,127 @@
 <template>
-  <div>
+  <div class="form">
     <b-card no-body>
       <b-tabs pills card>
         <b-tab title="Login" active>
-          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-            <b-form-group horizontal breakpoint="lg" label="Email:" :label-cols="2" label-for="email">
-              <b-form-input id="email"
-                            type="email"
-                            v-model="login.email"
-                            required
-                            placeholder="Enter email">
-              </b-form-input>
-            </b-form-group>
-            <b-form-group horizontal breakpoint="lg" :label-cols="2" label="Password:" label-for="password">
-              <b-form-input id="exampleInput2"
-                            type="password"
-                            v-model="login.password"
-                            required
-                            placeholder="Enter password">
-              </b-form-input>
-            </b-form-group>
-
-            <b-button type="submit" variant="primary">Login</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
-          </b-form>
+          <b-card bg-variant="light">
+            <b-form @submit="signin">
+              <b-form-group horizontal
+                            breakpoint="lg"
+                            label-class="text-sm-right"
+                            :label-cols="4"
+                            label-for="login.email">
+                <b-input-group prepend="Email:">
+                  <b-form-input id="login.email"
+                                type="email"
+                                v-model="login.email"
+                                required
+                                placeholder="Email eingeben">
+                  </b-form-input>
+                </b-input-group>
+              </b-form-group>
+              <b-form-group horizontal
+                            breakpoint="lg"
+                            label-class="text-sm-right"
+                            :label-cols="4"
+                            label-for="login.passwort"
+                            class="mb-0">
+                <b-input-group prepend="Passwort:">
+                  <b-form-input id="login.passwort"
+                                type="password"
+                                v-model="login.password"
+                                required
+                                placeholder="Passwort eingeben">
+                  </b-form-input>
+                </b-input-group>
+              </b-form-group>
+              <b-button class="mt-4" style="float: right" type="submit" variant="primary">Login</b-button>
+            </b-form>
+          </b-card>
         </b-tab>
-        <b-tab title="Register">
-          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-            <b-form-group id="exampleInputGroup1"
-                          label="Email address:"
-                          label-for="exampleInput1"
-                          description="We'll never share your email with anyone else.">
-              <b-form-input id="exampleInput1"
-                            type="email"
-                            v-model="form.email"
-                            required
-                            placeholder="Enter email">
-              </b-form-input>
+        <b-tab title="Register" style="overflow: hidden">
+          <b-form @submit="register">
+            <b-card bg-variant="light" >
+              <b-form-group horizontal
+                            breakpoint="lg"
+                            label="Anmeldedaten"
+                            label-class="font-weight-bold pt-0"
+                            class="mb-0">
+              <b-form-group horizontal
+                            breakpoint="lg"
+                            label-class="text-sm-right"
+                            :label-cols="3"
+                            label="Email:"
+                            label-for="email.register">
+                <b-form-input id="email.register"
+                              type="email"
+                              v-model="form.email"
+                              required
+                              placeholder="Email eingeben">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group horizontal
+                            breakpoint="lg"
+                            label-class="text-sm-right"
+                            :label-cols="3"
+                            label="Passwort:"
+                            label-for="passwort.register"
+                            class="mb-0">
+                <b-form-input id="passwort.register"
+                              type="password"
+                              v-model="form.password"
+                              required
+                              placeholder="Passwort eingeben">
+                </b-form-input>
+              </b-form-group>
             </b-form-group>
-            <b-form-group id="exampleInputGroup2"
-                          label="Your Name:"
-                          label-for="exampleInput2">
-              <b-form-input id="exampleInput2"
-                            type="text"
-                            v-model="form.name"
-                            required
-                            placeholder="Enter name">
-              </b-form-input>
-            </b-form-group>
-            <b-form-group id="titel"
-                          label="Titel:"
-                          label-for="titel">
-              <b-form-select id="titel"
-                            :options="titel"
-                            required
-                            v-model="form.titel">
-              </b-form-select>
-            </b-form-group>
-            <b-button type="submit" variant="primary">Register</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
+            </b-card>
+            <br/>
+            <b-card bg-variant="light">
+              <b-form-group horizontal
+                            breakpoint="lg"
+                            label="Pers&ouml;nliche Daten"
+                            label-class="font-weight-bold pt-0"
+                            class="mb-0">
+                <b-form-group horizontal
+                              breakpoint="lg"
+                              label-class="text-sm-right"
+                              :label-cols="3"
+                              label="Titel:"
+                              label-for="title" >
+                  <b-form-select id="title"
+                                 :options="titles"
+                                 v-model="form.title">
+                  </b-form-select>
+                </b-form-group>
+                <b-form-group horizontal
+                              breakpoint="lg"
+                              label-class="text-sm-right"
+                              :label-cols="3"
+                              label="Vorname:"
+                              label-for="register.firstname">
+                  <b-form-input id="register.firstname"
+                                type="text"
+                                v-model="form.firstName"
+                                required
+                                placeholder="Vorname eingeben">
+                  </b-form-input>
+                </b-form-group>
+                <b-form-group horizontal
+                              breakpoint="lg"
+                              label-class="text-sm-right"
+                              :label-cols="3"
+                              label="Nachname:"
+                              label-for="register.lastname">
+                  <b-form-input id="register.lastname"
+                                type="text"
+                                v-model="form.lastName"
+                                required
+                                placeholder="Nachname eingeben">
+                  </b-form-input>
+                </b-form-group>
+              </b-form-group>
+            </b-card>
+            <b-button class="mt-4" style="float:right" type="submit" variant="primary">Register</b-button>
           </b-form>
         </b-tab>
       </b-tabs>
@@ -77,36 +140,33 @@ export default {
         },
         form: {
         email: '',
-        name: '',
-        titel: null,
-        checked: []
+        password: '',
+        title: null,
+        firstName: '',
+        lastName: ''
       },
-      titel: [
-        { text: 'Select One', value: null },
+      titles: [
+        { text: 'optional', value: null },
         'Prof.', 'Dr.'
-      ],
-      show: true
+      ]
       }
     },
     methods: {
-    onSubmit (evt) {
-      evt.preventDefault();
-      alert(JSON.stringify(this.form));
+    signin (event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.login));
     },
-    onReset (evt) {
-      evt.preventDefault();
-      /* Reset our form values */
-      this.form.email = '';
-      this.form.name = '';
-      this.form.food = null;
-      this.form.checked = [];
-      /* Trick to reset/clear native browser form validation state */
-      this.show = false;
-      this.$nextTick(() => { this.show = true });
+    register (event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
     }
   }
 }
 </script>
 
 <style>
+  .form {
+    max-width: 60%;
+    margin: auto auto;
+  }
 </style>
