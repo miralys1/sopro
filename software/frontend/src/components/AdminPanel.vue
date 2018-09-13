fe5b40615cbae0264be4fda9c59097b6b4a2c8ef
Johanna <jo-hanna_menzel@live.de>
2018-09-13 13:58:25 +0200

    Mergefehler behoben

--- software/frontend/src/components/AdminPanel.vue
+++ software/frontend/src/components/AdminPanel.vue
@@ -2,14 +2,14 @@
   <div class="mainlayout">
   <b-card no-body>
   <b-tabs pills card vertical>
-    <b-tab title="Load Services from JSON" active>
-      <JsonEinlesen />
+    <b-tab title="Dienste aus JSON landen" active @click="toggle">
+      <JsonEinlesen v-if="toggly"/>
     </b-tab>
-    <b-tab title="Manually entering a service">
-      <AdminEingDienste />
+    <b-tab title="Dienst manuell bearbeiten" @click="toggle">
+      <AdminDienstForm />
     </b-tab>
-    <b-tab title="Tab 3">
-      Tab Contents 3
+    <b-tab title="Dienste bearbeiten" @click="toggleUpdate">
+      <AdminDienstBearbeiten v-if="toggly" v-bind:pupdate=update />
     </b-tab>
   </b-tabs>
 </b-card>
@@ -17,15 +17,32 @@
 </template>
 
 <script>
-import AdminEingDienste from '@/components/AdminEingDienste'
+import AdminDienstForm from '@/components/AdminDienstForm'
 import JsonEinlesen from '@/components/JsonEinlesen'
+import AdminDienstBearbeiten from '@/components/AdminDienstBearbeiten'
 
 export default {
   data() {
     return {
+      toggly: true,
+      update: true
     }
   },
-   components: { AdminEingDienste, JsonEinlesen
+   components: { AdminDienstForm, JsonEinlesen, AdminDienstBearbeiten
+
+   },
+   methods: {
+     toggle() {
+       this.toggly = false;
+       this.$nextTick(() => { this.toggly = true });
+     },
+
+     toggleUpdate() {
+       this.toggle;
+       this.update = !this.update;
+
+     }
+
 
    }
 }
