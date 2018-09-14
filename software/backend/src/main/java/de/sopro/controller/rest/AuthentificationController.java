5c3ea16747b5912d896cb313a5261c441bd600b6
Jeremia <stu122494@mail.uni-kiel.de>
2018-09-14 10:04:19 +0200

    HRS3105B-61: /authetification implementiert

---
+++ software/backend/src/main/java/de/sopro/controller/rest/AuthentificationController.java
@@ -0,0 +1,38 @@
+package de.sopro.controller.rest;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.sopro.model.User;
+import de.sopro.model.send.DetailUser;
+import de.sopro.repository.UserRepository;
+
+@RestController
+public class AuthentificationController{
+
+    @Autowired
+    UserRepository userRepo;
+
+    @RequestMapping(value="/authentification", method=RequestMethod.GET)
+    public ResponseEntity<DetailUser> getUserInformation(Principal principal){
+        if(principal == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        User user = userRepo.findByEmail(principal.getName());
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        DetailUser dUser = user.createDetailUser();
+        return new ResponseEntity<>(dUser, HttpStatus.OK);
+    }
+
+
+}
