e9d15891619b960bb5980ceff6988302c50b3271
Jeremia <stu122494@mail.uni-kiel.de>
2018-09-14 11:15:27 +0200

    HRS3105B-61: cors aktiviert

--- software/backend/src/main/java/de/sopro/controller/rest/AuthentificationController.java
+++ software/backend/src/main/java/de/sopro/controller/rest/AuthentificationController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class AuthentificationController{
     @Autowired
     UserRepository userRepo;
 
+    @CrossOrigin
     @RequestMapping(value="/authentification", method=RequestMethod.GET)
     public ResponseEntity<DetailUser> getUserInformation(Principal principal){
         if(principal == null){
