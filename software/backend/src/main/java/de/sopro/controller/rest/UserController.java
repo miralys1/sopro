5ff29c93c96561c9dfaf39cc09bb431dfddb1b6b
Jette <klette-99@kabelmail.de>
2018-09-14 10:45:07 +0200

    HRS3105B-89: getUser + getDetailUser

---
+++ software/backend/src/main/java/de/sopro/controller/rest/UserController.java
@@ -0,0 +1,68 @@
+package de.sopro.controller.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.sopro.model.Service;
+import de.sopro.model.User;
+import de.sopro.model.send.DetailUser;
+import de.sopro.model.send.SendService;
+import de.sopro.model.send.SimpleUser;
+import de.sopro.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ResponseEntity<Iterable<SimpleUser>> getUsers(
+			@RequestParam(value = "search", defaultValue = "") String searchString) {
+		Iterable<User> user = userRepo.findAll();
+		List<SimpleUser> simpUsers = new ArrayList<>();
+
+		for (User u : user) {
+			if (u.getFullName().contains(searchString) || searchString.equals("")) {
+				simpUsers.add(u.createSimpleUser());
+			}
+		}
+		Iterable<SimpleUser> result = simpUsers;
+
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+	public ResponseEntity<DetailUser> getUserDetails(@PathVariable long id) {
+		Optional<User> user = userRepo.findById(id);
+		if (!user.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		User u = user.get();
+		return new ResponseEntity<>(u.createDetailUser(), HttpStatus.OK);
+	}
+
+//	@RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
+//	public ResponseEntity<Void> editUser(@RequestParam(value = "user", defaultValue = "") DetailUser user,
+//			@PathVariable long id) {
+//		Optional<User> existingUser = userRepo.findById(id);
+//		if (!existingUser.isPresent()) {
+//			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//		}
+//
+//		user.setId(id);
+//		userRepo.save(user.createUser());
+//		return new ResponseEntity<>(HttpStatus.OK);
+//	}
+
+}
