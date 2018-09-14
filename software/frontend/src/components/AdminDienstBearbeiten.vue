c7ac6dff5d1c14b7c534fb3ba92465b4334e0153
Johanna <jo-hanna_menzel@live.de>
2018-09-14 13:37:59 +0200

    HRS3105B-80 Komma ergänzt

--- software/frontend/src/components/AdminDienstBearbeiten.vue
+++ software/frontend/src/components/AdminDienstBearbeiten.vue
@@ -57,7 +57,20 @@ methods: {
   },
 
   search() {
-    
+    var options = {
+    shouldSort: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+    "name",
+    "organisation"
+    ]
+};
+var fuse = new Fuse(this.services, options); // "list" is the item array
+this.foundServices = fuse.search(searchedService);
   }
 
 
