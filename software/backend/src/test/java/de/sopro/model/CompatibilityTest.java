315958003d262f3ed03e8f916a656d54eb11952d
Jette <klette-99@kabelmail.de>
2018-09-19 12:47:54 +0200

    HRS3105B-48: vergessene klasse ist jz auch gestged ^^

--- software/backend/src/test/java/de/sopro/model/CompatibilityTest.java
+++ software/backend/src/test/java/de/sopro/model/CompatibilityTest.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import de.sopro.model.send.CompatibilityAnswer;
 import de.sopro.repository.FormatRepository;
 import de.sopro.repository.ServiceRepository;
+import de.sopro.repository.TagRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,6 +29,9 @@ public class CompatibilityTest {
 	@Autowired
 	FormatRepository formatRepo;
 	
+	@Autowired
+	TagRepository tagRepo;
+
 	@Autowired
 	Compatibility compa;
 
@@ -153,18 +157,69 @@ public class CompatibilityTest {
 		formatRepo.delete(fOut);
 	}
 
+	@Test
+	public void notCompatibleAlternativeServices() {
+		List<Tag> tags = new ArrayList<>();
+
+		Format fOut = new Format("FBX", "2.0", "flexible");
+		Format fCIn = new Format("FBX", "2.0", "flexible");
+		Format fCOut = new Format("IFC", "4", "strict");
+		Format fIn = new Format("IFC", "4", "strict");
+		formatRepo.save(fOut);
+		formatRepo.save(fCIn);
+		formatRepo.save(fCOut);
+		formatRepo.save(fIn);
+
+		Service s1 = createService1(tags, fOut);
+		serviceRepo.save(s1);
+		Service s2 = createService2(tags, fIn);
+		serviceRepo.save(s2);
+		Service conv = createConverter(tags, fCOut, fCIn);
+		serviceRepo.save(conv);
+
+		CompatibilityAnswer answer = compa.checkCompatibility(s1.getId(), s2.getId());
+		assertFalse(answer.isCompatible());
+		assertTrue(answer.getSuitingFormats().size() == 0);
+		assertTrue(answer.getCompatibleServices().size() == 1);
+//				&& answer.getCompatibleServices().get(0).getNames().get(0).equals("FBX-IFC Converter"));
+
+		serviceRepo.delete(s1);
+		serviceRepo.delete(s2);
+		serviceRepo.delete(conv);
+		formatRepo.delete(fIn);
+		formatRepo.delete(fCIn);
+		formatRepo.delete(fCOut);
+		formatRepo.delete(fOut);
+	}
+
+	// // // // // // //
+	// Helper Methods //
+	// // // // // // //
+	
 	private Service createService2(List<Tag> tags, Format fIn) {
-		List<Format> formatIn2 = new ArrayList<>();
-		List<Format> formatOut2 = new ArrayList<>();
-		formatIn2.add(fIn);
-		return new Service("3D-Modeller", "3", tags, "IGD", 1531573788, "IGD_Modeller.png", formatIn2, formatOut2);
+		List<Format> formatIn = new ArrayList<>();
+		List<Format> formatOut = new ArrayList<>();
+		formatIn.add(fIn);
+		return new Service("3D-Modeller", "3", tags, "IGD", 1531573788, "IGD_Modeller.png", formatIn, formatOut);
 	}
 
 	private Service createService1(List<Tag> tags, Format fOut) {
-		List<Format> formatIn1 = new ArrayList<>();
-		List<Format> formatOut1 = new ArrayList<>();
-		formatOut1.add(fOut);
-		return new Service("TP Modeller", "1.0", tags, "TP", 153443388, "TP_Modeller_10.png", formatIn1, formatOut1);
+		List<Format> formatIn = new ArrayList<>();
+		List<Format> formatOut = new ArrayList<>();
+		formatOut.add(fOut);
+		return new Service("TP Modeller", "1.0", tags, "TP", 153443388, "TP_Modeller_10.png", formatIn, formatOut);
+	}
+
+	private Service createConverter(List<Tag> tags, Format fOut, Format fIn) {
+		List<Format> formatIn = new ArrayList<>();
+		List<Format> formatOut = new ArrayList<>();
+		formatOut.add(fOut);
+		formatIn.add(fIn);
+		Tag t = new Tag("Converter");
+		tagRepo.save(t);
+		tags.add(t);
+		return new Service("FBX-IFC Converter", "1.0", tags, "BIM Converter", 1490965788, "Converter_FBX_IFC.png",
+				formatIn, formatOut);
 	}
 
 }
