bda2810addc14d0721354ee0016b50ed044aa199
Jette <klette-99@kabelmail.de>
2018-09-19 21:57:03 +0200

    HRS3105B-100: CompositionEdge kommentiert

--- software/backend/src/main/java/de/sopro/model/CompositionEdge.java
+++ software/backend/src/main/java/de/sopro/model/CompositionEdge.java
@@ -10,9 +10,16 @@ import javax.validation.constraints.NotNull;
 
 import de.sopro.model.send.Edge;
 
+/**
+ * A CompositionEdge represents a connection between two CompositionNodes
+ * 
+ * @author HRS3-R.105B
+ *
+ */
 @Entity
 public class CompositionEdge {
 
+	/* required attributes */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -30,11 +37,20 @@ public class CompositionEdge {
 
 	}
 
+	/**
+	 * creates a CompositionEdge with {@code source} and {@code target}
+	 * 
+	 * @param source
+	 *            origin CompositionNode of the CompositionEdge
+	 * @param target
+	 *            target CompositionNode of the CompositionEdge
+	 */
 	public CompositionEdge(CompositionNode source, CompositionNode target) {
 		this.source = source;
 		this.target = target;
 	}
 
+	/* getter and setters */
 	public CompositionNode getSource() {
 		return source;
 	}
@@ -59,11 +75,19 @@ public class CompositionEdge {
 		return id;
 	}
 
+	/**
+	 * converts a CompositionEdge to an Edge
+	 * 
+	 * @return an Edge that represents the CompositionEdge
+	 */
 	public Edge createEdge() {
 		Edge e = new Edge(this.id, this.source.createNode(), this.target.createNode());
 		return e;
 	}
 
+	/**
+	 * converts the CompositionEdge to a String
+	 */
 	public String toString() {
 		return source + " -> " + target;
 	}
