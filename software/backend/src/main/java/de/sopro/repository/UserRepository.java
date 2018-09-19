fe3cdf931ebc0079ac5c3e32893bf44fe99fa3e2
Jette <klette-99@kabelmail.de>
2018-09-19 22:21:47 +0200

    HRS3105B-100: Repositories kommentiert

--- software/backend/src/main/java/de/sopro/repository/UserRepository.java
+++ software/backend/src/main/java/de/sopro/repository/UserRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.repository.CrudRepository;
 
 import de.sopro.model.User;
 
+/**
+ * Repository for Users
+ * 
+ * @author HRS3-R.105B
+ *
+ */
 public interface UserRepository extends CrudRepository<User, Long> {
-  
-  User findByEmail(String email);
+
+	/* additional method for searching the repository */
+	User findByEmail(String email);
 
 }
