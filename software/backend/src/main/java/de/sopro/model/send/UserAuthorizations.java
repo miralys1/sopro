f67f97f43a93ccc3214512ab53c6431ed721b831
Jette <klette-99@kabelmail.de>
2018-09-20 12:30:13 +0200

    HRS3105B-100: SimpleUser & UserAuth kommentiert

--- software/backend/src/main/java/de/sopro/model/send/UserAuthorizations.java
+++ software/backend/src/main/java/de/sopro/model/send/UserAuthorizations.java
@@ -2,16 +2,33 @@ package de.sopro.model.send;
 
 import java.util.List;
 
+/**
+ * An UserAuthorizations instance contains information about the permissions for
+ * a composition.
+ * 
+ * @author HRS3-R.105B
+ *
+ */
 public class UserAuthorizations {
 
+	/* required variables */
 	private List<SimpleUser> editors;
 	private List<SimpleUser> viewers;
 
+	/**
+	 * creates an UserAuthorizations with the given values
+	 * 
+	 * @param editors
+	 *            User that have the permission to edit
+	 * @param viewers
+	 *            User that have the permission to view
+	 */
 	public UserAuthorizations(List<SimpleUser> editors, List<SimpleUser> viewers) {
 		this.editors = editors;
 		this.viewers = viewers;
 	}
 
+	/* getetr and setter */
 	public List<SimpleUser> getEditors() {
 		return editors;
 	}
@@ -28,6 +45,9 @@ public class UserAuthorizations {
 		this.viewers = viewers;
 	}
 
+	/**
+	 * converts the UserAuthorizations to a String
+	 */
 	public String toString() {
 		String s = "editors: ";
 		for (int i = 0; i < editors.size() - 1; i++) {
